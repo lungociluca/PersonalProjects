2a11f6ff6544f452ea2298982aa7cfa2ab617111
lungociluca <79647600+lungociluca@users.noreply.github.com>
2022-01-20 19:35:24 +0200

    Add files via upload

---
+++ Website/backend/ecommerce/src/main/java/com/se/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,17 @@
+package com.se.ecommerce.dao;
+
+import com.se.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@CrossOrigin("http://localhost:4200") //accept calls from web browser scripts
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    //query method: match by category id & REST will automatically expose that endpoint
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+
+    Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
+}
