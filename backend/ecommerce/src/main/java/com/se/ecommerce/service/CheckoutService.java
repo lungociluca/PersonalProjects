506ad1383f8089c569cb863a56e1243b4c906979
lungociluca <79647600+lungociluca@users.noreply.github.com>
2022-01-20 19:33:24 +0200

    Delete backend/ecommerce directory

--- backend/ecommerce/src/main/java/com/se/ecommerce/service/CheckoutService.java
+++
@@ -1,9 +0,0 @@
-package com.se.ecommerce.service;
-
-import com.se.ecommerce.dto.Purchase;
-import com.se.ecommerce.dto.PurchaseResponse;
-
-public interface CheckoutService {
-
-    PurchaseResponse placeOrder(Purchase purchase);
-}
