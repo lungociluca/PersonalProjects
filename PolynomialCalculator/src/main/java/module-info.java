e050120cbcd33a52e4f237240ece8c0bc332a878
lungociluca <79647600+lungociluca@users.noreply.github.com>
2022-03-13 17:15:15 +0200

    Delete PolynomialCalculator directory

--- PolynomialCalculator/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires junit;
-
-    opens GUI to javafx.fxml;
-    exports  test to junit;
-    exports GUI;
-}
